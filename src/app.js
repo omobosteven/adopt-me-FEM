b4102d0257253519d2e659beb3515cb983c46a79
omobosteven <omobosteven@gmail.com>
2020-08-21 15:12:23 +0100

    add react portal and context

--- src/app.js
+++ src/app.js
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
+import { Link, Router } from "@reach/router";
 
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
-  ]);
+  const themeHook = useState("peru");
+  return (
+    <React.StrictMode>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
+    </React.StrictMode>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
